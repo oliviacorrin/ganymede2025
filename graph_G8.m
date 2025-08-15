648f5bad67b83c0100edea770ac274b0a0140e75
oliviacorrin <ocorrin@uoregon.edu>
2025-08-14 18:20:11 -0700

    new file to graph G8 trajectory
    magnetopause crossing is visible in change of B field vectors

---
+++ graph_G8.m
@@ -0,0 +1,43 @@
+% Graph spacecraft trajectory of G8 
+
+% File name G8 with delimeterIn = , 
+% headerlinesIn = 1 (start at 2nd column so we don't import the dates)
+G8 = importdata('Galileo_O8.csv',',',2)
+
+% create trajectory array with each column representing an x,y,z component of
+% the spacecraft trajectory
+trajectory = zeros(length(G8.data),3)
+for i = 1:3
+    trajectory(:,i) = G8.data(:,i+4)
+end
+
+% spacecraft-measured B field values
+B_field = zeros(length(G8.data),3)
+for i = 1:3
+    B_field(:,i) = G8.data(:,i)
+end
+
+% create 3d plot of trajectory 
+plot3(trajectory(:,1),trajectory(:,2),trajectory(:,3),'-b')
+xlabel("x (in R_g)")
+ylabel("y (in R_g)")
+zlabel("z (in R_g)")
+title("G8 trajectory practice graph")
+hold on
+% add Ganymede at origin 
+r_factor = 0.038 % multiply by R_g/R_j
+[X,Y,Z] = sphere
+X2 = X*r_factor
+Y2 = Y*r_factor
+Z2 = Z*r_factor 
+% assuming R_j 
+% G1_plot = surf(X2,Y2,Z2)
+
+% assuming R_g
+G8_plot = surf(X,Y,Z)
+G8_plot.FaceColor = [0.25 0.8 0.7]
+legend("trajectory", "Ganymede")
+axis equal
+
+% add quiver plot to show magnitude/direction of B field 
+quiver3(trajectory(:,1),trajectory(:,2),trajectory(:,3),B_field(:,1),B_field(:,2),B_field(:,3))
