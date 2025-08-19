9f89217bfdfcd24d4935eee8f868f81a8c81b047
oliviacorrin <ocorrin@uoregon.edu>
2025-08-19 13:28:09 -0700

    added tentative magnetopause crossing boundaries

--- graph_G8.m
+++ graph_G8.m
@@ -1,4 +1,7 @@
-% Graph spacecraft trajectory of G8 
+%% Graph spacecraft trajectory of G8 
+clc
+close all
+clear all
 
 % File name G8 with delimeterIn = , 
 % headerlinesIn = 1 (start at 2nd column so we don't import the dates)
@@ -17,7 +20,7 @@ for i = 1:3
     B_field(:,i) = G8.data(:,i)
 end
 
-% create 3d plot of trajectory 
+%% create 3d plot of trajectory 
 plot3(trajectory(:,1),trajectory(:,2),trajectory(:,3),'-b')
 xlabel("x (in R_g)")
 ylabel("y (in R_g)")
@@ -39,18 +42,34 @@ G8_plot.FaceColor = [0.25 0.8 0.7]
 legend("trajectory", "Ganymede")
 axis equal
 
-% add quiver plot to show magnitude/direction of B field 
+%% add quiver plot to show magnitude/direction of B field 
 quiver3(trajectory(:,1),trajectory(:,2),trajectory(:,3),B_field(:,1),B_field(:,2),B_field(:,3))
 hold off
 
-% plot B field time series
-% first plot the 3 components (in GPhiO)
+%% plot B field time series
+% plot the components (in GPhiO)
 plot(B_field(:,1))
 hold on
 plot(B_field(:,2))
 plot(B_field(:,3))
+% add the magnitude 
 plot(G8.data(:,4))
-xlabel("time elapsed (units?)")
+xlabel("time elapsed (# of data points out of total taken)")
 ylabel("B field strength (nT)")
 title("G8 flyby magnetometer measurements")
-legend("B_x","B_y","B_z","B (mag)")
+
+%% add apparent boundaries of magnetopause corresponding to sharp changes in
+%% B field (all eyeballed by yours truly)
+% first apparent boundary
+xline(2284,'-k','LineWidth',2)
+xline(4586,'-k','LineWidth',2)
+% possible bounds to the entrance crossing 
+xline(2132,'--','Color',[0 .8 .8],'LineWidth',2)
+xline(2680,'--','Color',[0 .8 .8],'LineWidth',2)
+% possible bounds to the exit crossing 
+xline(4728,'--','Color',[0 .8 .8],'LineWidth',2)
+xline(4159,'--','Color',[0 .8 .8],'LineWidth',2)
+legend("B_x","B_y","B_z","B (mag)",'mpause entrance crossing','mpause exit crossing','possible min/max crossing bounds')
+
+
+
